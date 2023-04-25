da021114a3f0fbed7b404b1875025acf5ccaf61d
Saplak <83984717+Saplak@users.noreply.github.com>
2023-04-24 23:19:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return 'Olá, mundo!'
+
+@app.route('/pagina1')
+def pagina1():
+    return render_template('pagina1.html')
+
+@app.route('/pagina2')
+def pagina2():
+    return render_template('pagina2.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
