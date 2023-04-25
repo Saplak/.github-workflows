c99efdd57f68a863310b08272a5fb7ad69128ebf
Saplak <83984717+Saplak@users.noreply.github.com>
2023-04-24 23:08:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 from flask import Flask
 
-app = Flask(app.py)
+app = Flask(__name__)
 
 @app.route('/')
-def home():
+def hello_world():
     return 'Hello, World!'
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
